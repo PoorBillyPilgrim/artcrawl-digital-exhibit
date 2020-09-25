d7ad98097ec2c759f7f0709e16b7e1720ee431bf
Tyler Jones <tjjones93@gmail.com>
2020-09-25 15:43:52 -0400

    add regex var, pass to ejs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const data = require('./data/data.json');
 const path = require('path');
+const regex = /\.jpeg|\.jpg|\.png/g;
+
 
 const app = express();
 // Must match JSON keys exactly, capiatlize for single words and use bracket notation for keys > one word
@@ -12,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { data: data });
+    res.render('index', { data: data, regex: regex });
 });
 
 app.listen(3000, () => {
