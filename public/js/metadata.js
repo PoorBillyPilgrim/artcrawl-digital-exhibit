67f3cc129b895d3a510b088590822b871aa63418
Tyler Jones <tjjones93@gmail.com>
2020-10-09 13:58:38 -0400

    add openseadragon lightbox

--- public/js/metadata.js
+++ public/js/metadata.js
@@ -28,8 +28,27 @@ function getImgSizeInfo(img) {
         parseInt(pos[0]));
 }
 
+
+
 // JQuery for #metadata div
 $(document).ready(function () {
+
+    $('.art-crawl-item').click(function () {
+        var viewer = OpenSeadragon({
+            id: 'openseadragon',
+            prefixUrl: '/images/openseadragon/images/',
+            tileSources: '/images/image0.dzi'
+        })
+        $('#openseadragon').toggleClass('show');
+        $('.artcrawl-container').addClass('hide');
+    })
+
+
+
+
+
+
+    console.log($('.grid-container').width());
     // on DOM load
     let gridItem, name, title, major, img, obj;
 
@@ -51,58 +70,58 @@ $(document).ready(function () {
 
 
 
-
-    $('.art-crawl-item').click(function () {
-        // get metadata
-        //let name, title, major, img;
-
-        name = $(this).attr('data-last-name'); // can create function to abstract and consolidate
-        title = $(this).attr('data-title');
-        major = $(this).attr('data-major');
-        img = $(this).find('img').attr('src');
-
-        // get calculated width after scale down
-        /*var obj;
-        document.querySelector('#metadata-img').addEventListener('load', function (e) {
-            obj = getImgSizeInfo(e.target);
-        });
-        obj = getImgSizeInfo(document.querySelector('#metadata-img'));
-
-        console.log(`Left: ${obj.left}px, Right: ${obj.right}px, Width: ${obj.width}`);*/
-
-        //$('#metadata-data').css('left', obj.left);
-        //$('#metadata-data').css('right', obj.right);
-
-        // if ($('#metadata').hasClass('active')) {
-        $('#metadata.active').css('opacity', 0);
-        setTimeout(function () {
-            $('#metadata-img').attr('src', img);
+    /*
+        $('.art-crawl-item').click(function () {
+            // get metadata
+            //let name, title, major, img;
+            /*
+            name = $(this).attr('data-last-name'); // can create function to abstract and consolidate
+            title = $(this).attr('data-title');
+            major = $(this).attr('data-major');
+            img = $(this).find('img').attr('src');
+    
+            // get calculated width after scale down
+            /*var obj;
+            document.querySelector('#metadata-img').addEventListener('load', function (e) {
+                obj = getImgSizeInfo(e.target);
+            });
             obj = getImgSizeInfo(document.querySelector('#metadata-img'));
-            $('#metadata-data').css('left', obj.left);
-            $('#metadata-data').css('max-width', obj.width);
-            $('#metadata-data').html('<strong>' + name + '</strong>' + ', ' + title + ', ' + major)
+    
+            console.log(`Left: ${obj.left}px, Right: ${obj.right}px, Width: ${obj.width}`);*/
 
-            $('#metadata.active').css('opacity', 1);
-        }, 550);
-        /*} else {
-            // render metadata in #metadata
-            $('#metadata-data').html('<strong>' + name + '</strong>' + ', ' + title + ', ' + major)
-            $('#metadata-img').attr('src', img);
-            $('#metadata').addClass('active');
-        }*/
+    //$('#metadata-data').css('left', obj.left);
+    //$('#metadata-data').css('right', obj.right);
+    /*
+    // if ($('#metadata').hasClass('active')) {
+    $('#metadata.active').css('opacity', 0);
+    setTimeout(function () {
+        $('#metadata-img').attr('src', img);
+        obj = getImgSizeInfo(document.querySelector('#metadata-img'));
+        $('#metadata-data').css('left', obj.left);
+        $('#metadata-data').css('max-width', obj.width);
+        $('#metadata-data').html('<strong>' + name + '</strong>' + ', ' + title + ', ' + major)
+
+        $('#metadata.active').css('opacity', 1);
+    }, 550);
+    /*} else {
+        // render metadata in #metadata
+        $('#metadata-data').html('<strong>' + name + '</strong>' + ', ' + title + ', ' + major)
+        $('#metadata-img').attr('src', img);
+        $('#metadata').addClass('active');
+    }
 
 
+    $('#openseadragon').addClass('show');
+    // orange highlight on click
+    if (!$(this).hasClass('highlight')) {
+        $('.art-crawl-item').removeClass('highlight');
+        $(this).addClass('highlight');
+    } else {
+        $(this).addClass('highlight');
+    }
 
-        // orange highlight on click
-        if (!$(this).hasClass('highlight')) {
-            $('.art-crawl-item').removeClass('highlight');
-            $(this).addClass('highlight');
-        } else {
-            $(this).addClass('highlight');
-        }
-
-    });
-
+});
+*/
     // hide #metadata
     /*
     $('#metadata-close').click(function () {
