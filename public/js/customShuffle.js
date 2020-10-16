ef1962d540428f8e855f62c54f8292b4f534cbbc
Tyler Jones <tjjones93@gmail.com>
2020-10-16 17:42:03 -0400

    add imageLoaded

--- public/js/customShuffle.js
+++ public/js/customShuffle.js
@@ -1,5 +1,5 @@
 var Shuffle = window.Shuffle;
-
+var grid;
 // https://codepen.io/Vestride/pen/ZVWmMX -- from Shuffle.js website
 class Grid {
     constructor(element) {
@@ -12,7 +12,17 @@ class Grid {
         });
 
         this.addSorting();
+        
+        // on img load
+        // https://github.com/Vestride/Shuffle/issues/100
+        var imgLoad = imagesLoaded(element);
+        imgLoad.on('always',this.onAlways.bind(this));
     }
+
+    onAlways() {
+        console.log('all images are loaded');
+        this.shuffle.layout();
+    };
 
     addSorting() {
         const btnGroup = document.querySelector('.sort-options');
@@ -63,6 +73,30 @@ class Grid {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.grid = new Grid(document.getElementById('grid'));
-    console.log('Shuffle grid loaded!')
+    //window.grid = new Grid(document.getElementById('grid'));
+    grid = new Grid(document.getElementById('grid'));
+    console.log('Shuffle grid loaded.')
 });
+var gridUpdate = debounce(function () {
+    grid.shuffle.update();
+    console.log('grid reset.')
+}, 250);
+
+// window.addEventListener('resize', gridUpdate);
+
+// debounce function to use with window resize
+// https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
