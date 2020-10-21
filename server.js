0e8221fbd5059ca801ef2ba61d18d679b2a74e00
Tyler Jones <tjjones93@gmail.com>
2020-10-21 13:09:11 -0400

    add dot-env

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const data = require('./data/data.json');
 const randomPhotos = require('./data/randomPhotos.json');
 const path = require('path');
 const regex = /\.jpeg|\.jpg|\.png/g;
 
+const port = process.env.PORT || 3000;
 
 const app = express();
 // Must match JSON keys exactly, capiatlize for single words and use bracket notation for keys > one word
@@ -23,6 +25,6 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.info('Listening on port 3000');
 });
