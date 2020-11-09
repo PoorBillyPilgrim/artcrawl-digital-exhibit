d3a35eca37448b4b3b9471115dfa6a6ca08f9c80
Tyler Jones <tjjones93@gmail.com>
2020-11-09 16:49:16 -0500

    add <a> in hero and animation

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,14 @@ $(function() {
     const Shuffle = window.Shuffle;
     let shuffle;
 
+    $('#hero > a').click(function(event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 1250);
+    })
+
     const grid = Grid.init();
     const metadata = Metadata.init();
 });
