e22db121bc4685609c4931d17739ecf38e9119a9
Tyler Jones <tjjones93@gmail.com>
2020-11-10 11:28:52 -0500

    add pushState() on click to change hash to first grid item

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,8 @@ $(function() {
         $('html, body').animate({
             scrollTop: $( $.attr(this, 'href') ).offset().top
         }, 1250);
+
+        history.pushState({'item_id': 0}, 'Art Crawl', window.location.hash = '#dziID=' + 0 + '&id=' + 0);
     })
 
     const grid = Grid.init();
