4a9b088a336d504972b3d116906f58f44747960b
Tyler Jones <tjjones93@gmail.com>
2020-11-10 11:41:09 -0500

    remove hero a tag event listener and add to metadata.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,6 @@ $(function() {
     const Shuffle = window.Shuffle;
     let shuffle;
 
-    $('#hero > a').click(function(event) {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 1250);
-
-        history.pushState({'item_id': 0}, 'Art Crawl', window.location.hash = '#dziID=' + 0 + '&id=' + 0);
-    })
-
     const grid = Grid.init();
     const metadata = Metadata.init();
 });
