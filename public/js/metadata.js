4620fb4f9ba5e65bde85c861aa3e036e9b127ea3
Tyler Jones <tjjones93@gmail.com>
2020-11-16 17:39:50 -0500

    change createHtmlOverlay to loop through arguments so that it can create an overlay with any number of metadata

--- public/js/metadata.js
+++ public/js/metadata.js
@@ -60,7 +60,7 @@ function renderMetadataImg($metadata, dataAttributes, $metadataImg = document.qu
         'left': imgSize.left,
         'max-width': imgSize.width
     });
-    $metadata.html('<strong>' + dataAttributes.name + '</strong>' + ', ' + dataAttributes.title + ', ' + dataAttributes.major)
+    $metadata.html('<em>' + dataAttributes.title + '</em>, ' + dataAttributes.name + ', ' + dataAttributes.major)
 }
 
 function debounce(func, wait, immediate) {
@@ -87,11 +87,18 @@ function handleHighlight() {
     }, 2700); // grid speed is set to 2650ms
 }
 
-function createHtmlOverlay(title, artist, description, major) {
+function createHtmlOverlay(title, artist,  major, description) {
     function p(text) {
         return '<p>' + text + '</p>'
     };
-    return p(title) + p(artist) + p(description) + p(major);
+    let html = "";
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(arguments[i])
+        if(arguments[i] != "") {
+            html += p(arguments[i]);
+        }
+    }
+    return html;
 }
 
 var Metadata = (function () {
@@ -182,7 +189,7 @@ var Metadata = (function () {
             gridItemID = $('.art-crawl-item.highlight').attr('id') || '0';
 
             $ID = $('#' + gridItemID);
-            let html = createHtmlOverlay($ID.attr('data-title'), $ID.attr('data-last-name'), $ID.attr('data-artist-statment'), $ID.attr('data-major'));
+            let html = createHtmlOverlay($ID.attr('data-title'), $ID.attr('data-last-name'), $ID.attr('data-major'), $ID.attr('data-artist-statement'));
             $('#html-overlay').append(html);
 
             $('#legend').addClass('hide');
@@ -221,11 +228,10 @@ var Metadata = (function () {
     }
 
     const renderMetadata = function() {
-        $('.art-crawl-item').click(function () {
+        $('.art-crawl-item').click(function (event) {
             
             dataAttributes = getDataAttributes($(this));
             img = $(this).find('img').attr('src');
-            //$('#metadata-span').css('background-color', '')
     
             $('#metadata.active').css('opacity', 0);
             setTimeout(function () {
@@ -237,17 +243,30 @@ var Metadata = (function () {
                 dziID = $('.art-crawl-item.highlight').attr('data-dzi-id');
                 history.pushState({'item_id': gridItemID}, 'Art Crawl', window.location.hash = '#dziID=' + dziID + '&id=' + gridItemID);
             }, 550);
-    
+            
+
+            /*function handleShowColor(selector, opacity) {
+                if(selector.classList.contains('show-color')) {
+                    selector.style.opacity = opacity;
+                }
+            }*/
             // orange highlight on click
             if (!$(this).hasClass('highlight')) {
+                //let imgBefore = document.querySelector('.highlight').firstElementChild;
+                //handleShowColor(imgBefore, 0) // doesn't work
+                // console.log($('.art-crawl-item.highlight'))
                 $('.art-crawl-item').removeClass('highlight');
                 $(this).addClass('highlight');
+                //let imgAfter = event.currentTarget.firstElementChild;
+                // console.log($('.art-crawl-item.highlight'))
+                //handleShowColor(imgAfter, 1); // doesn't work
             } else {
                 $(this).addClass('highlight');
             }
 
             let activeColor = $('.art-crawl-item.highlight').css('background-color');
             $('#legend').css({'background-color': activeColor});
+
 
             
         });
@@ -292,7 +311,18 @@ var Metadata = (function () {
         let colors = {"Industrial Design":{"count":2,"color":"#f257c1"},"BME":{"count":5,"color":"#2f538e"},"Mechanical Engineering":{"count":13,"color":"#4c4eb2"},"Physics and LMC":{"count":1,"color":"#e076d2"},"Computer Science":{"count":18,"color":"#295b93"},"Architecture":{"count":6,"color":"#b4fcae"},"Chemical & Biomolecular Engineering":{"count":1,"color":"#83fc9d"},"Industrial Engineering":{"count":3,"color":"#6d22b7"},"LMC":{"count":1,"color":"#ef5f7a"},"Electrical Engineering":{"count":3,"color":"#db6087"},"Aerospace":{"count":1,"color":"#9f63ed"},"Master of City and Regional Planning":{"count":1,"color":"#04edbe"},"Neuroscience":{"count":5,"color":"#d863c5"},"physics":{"count":1,"color":"#e1e569"},"Computer Science (PhD)":{"count":1,"color":"#3ffc8a"},"Biomedical Engineering":{"count":10,"color":"#ba5d1b"},"CS":{"count":1,"color":"#5df4e5"},"Civil Engineering":{"count":3,"color":"#b0f716"},"Aerospace Engineering":{"count":5,"color":"#ffba66"},"Architect":{"count":2,"color":"#00c197"},"Biochemistry":{"count":3,"color":"#af68ed"},"ME":{"count":4,"color":"#b7daf7"},"Biomedical Engineering & ALIS":{"count":1,"color":"#fcdf6c"},"Industrial & Systems Engineering":{"count":1,"color":"#cc186c"},"Business Administration":{"count":4,"color":"#df6cfc"},"Chemical Engineering":{"count":2,"color":"#ed2bff"},"Biology":{"count":4,"color":"#f9978e"},"Robotics":{"count":1,"color":"#ef6eb3"},"Bioinformatics":{"count":1,"color":"#c67d0f"},"English":{"count":1,"color":"#359099"},"Computer Engineer":{"count":1,"color":"#e0003f"},"ALIS":{"count":1,"color":"#28cc7f"},"Mathematics":{"count":1,"color":"#5d22cc"},"Computer Engineering":{"count":2,"color":"#98e559"},"Materials Science and Engineering":{"count":2,"color":"#ed6a9a"},"BMED":{"count":1,"color":"#d0afed"},"MSE":{"count":1,"color":"#f699ff"},"EE":{"count":1,"color":"#d33295"},"Online Master of Science in Analytics":{"count":1,"color":"#dc59f9"},"ECE":{"count":1,"color":"#75efe5"},"ARCHITECTURE":{"count":1,"color":"#3c36b5"},"HTS":{"count":1,"color":"#6be845"},"M.S. Global Media and Cultures":{"count":1,"color":"#8de8c0"},"Physics":{"count":2,"color":"#55db57"},"Computational Media":{"count":2,"color":"#3f70af"},"Master of Architecture":{"count":1,"color":"#ea4c3a"},"Environmental Engineering":{"count":2,"color":"#edb0a8"},"Literature, Media and Communication":{"count":1,"color":"#b516f4"},"AE":{"count":1,"color":"#b9ed95"},"Chemical and Biomolecular Engineering":{"count":1,"color":"#d3ef97"},"Literature, Media, and Communication":{"count":1,"color":"#f4c6ff"},"Psychology":{"count":1,"color":"#f9eeae"}};
 
         $('#color-btn').click(function() {
-            $('.art-crawl-item > img').toggleClass('active');
+            
+            let hlImg = document.querySelector('.highlight').firstElementChild;
+            let item  = document.querySelector('.art-crawl-item');
+           // console.log(img.classList.contains('show-color'))
+            $('.art-crawl-item > img').toggleClass('show-color');
+            /* if(hlImg.classList.contains('show-color')) {
+                hlImg.style.opacity = 0;
+                console.log(hlImg.style.opacity)
+            } else {
+                hlImg.style.opacity = 1;
+            } */
+            // $('.art-crawl-item.highlight > img').css('opacity', 1); // need to toggle
             $('#legend').toggleClass('hide');
 
             let figures = document.querySelectorAll('.art-crawl-item');
