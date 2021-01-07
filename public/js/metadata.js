a7294be393525ccf746896b6b80e7d670b712b25
Tyler Jones <tjjones93@gmail.com>
2021-01-06 21:24:37 -0500

    change #legend to always display

--- public/js/metadata.js
+++ public/js/metadata.js
@@ -209,9 +209,7 @@ var Metadata = (function () {
             $('#openseadragon-close').addClass('hide');
             $('.artcrawl-container').removeClass('hide');
             $('#info').removeClass('hide');
-            if($('.art-crawl-item > img').hasClass('show-color')) {
-                $('#legend').removeClass('hide');
-            }
+            $('#legend').removeClass('hide');
             
             viewer.destroy();
             viewer = null;
@@ -338,19 +336,23 @@ var Metadata = (function () {
     const renderColor = function() {
         let colors = {"Industrial Design":{"count":3,"color":"#f7aaf2"},"BME":{"count":5,"color":"#ea7e90"},"Mechanical Engineering":{"count":13,"color":"#28e257"},"Physics and LMC":{"count":1,"color":"#2b3bef"},"Computer Science":{"count":18,"color":"#daa2e8"},"Architecture":{"count":6,"color":"#152487"},"Chemical & Biomolecular Engineering":{"count":1,"color":"#8015b2"},"Industrial Engineering":{"count":3,"color":"#f2756f"},"LMC":{"count":1,"color":"#fc562d"},"Electrical Engineering":{"count":3,"color":"#577fd6"},"Aerospace":{"count":1,"color":"#abbce8"},"Master of City and Regional Planning":{"count":1,"color":"#fcc7d3"},"Neuroscience":{"count":5,"color":"#349fb5"},"physics":{"count":1,"color":"#60db8d"},"Computer Science (PhD)":{"count":1,"color":"#e086de"},"Biomedical Engineering":{"count":10,"color":"#9ff4e3"},"CS":{"count":1,"color":"#efca0e"},"Civil Engineering":{"count":3,"color":"#efb3ac"},"Aerospace Engineering":{"count":6,"color":"#ff9bb2"},"Architect":{"count":2,"color":"#f8c6ff"},"Biochemistry":{"count":3,"color":"#9e7ed6"},"ME":{"count":4,"color":"#fcdfbf"},"Biomedical Engineering & ALIS":{"count":1,"color":"#4b92f4"},"Industrial & Systems Engineering":{"count":1,"color":"#230489"},"Business Administration":{"count":5,"color":"#f9c866"},"Chemical Engineering":{"count":2,"color":"#ce27bd"},"Biology":{"count":4,"color":"#d99df9"},"Robotics":{"count":1,"color":"#1eb573"},"Bioinformatics":{"count":1,"color":"#ef757f"},"English":{"count":1,"color":"#9def4a"},"Computer Engineer":{"count":1,"color":"#6cfcce"},"ALIS":{"count":1,"color":"#f7bffc"},"Mathematics":{"count":1,"color":"#232b82"},"Computer Engineering":{"count":2,"color":"#8bf4b0"},"Materials Science and Engineering":{"count":2,"color":"#32bc37"},"BMED":{"count":1,"color":"#0f9e33"},"MSE":{"count":1,"color":"#ead162"},"EE":{"count":1,"color":"#db8967"},"Online Master of Science in Analytics":{"count":1,"color":"#b1deef"},"ECE":{"count":1,"color":"#18036d"},"ARCHITECTURE":{"count":1,"color":"#daa9f2"},"HTS":{"count":1,"color":"#176e82"},"M.S. Global Media and Cultures":{"count":1,"color":"#a295e2"},"Physics":{"count":2,"color":"#2dc922"},"Computational Media":{"count":2,"color":"#37efcd"},"Master of Architecture":{"count":1,"color":"#c69ce2"},"Environmental Engineering":{"count":2,"color":"#2eb71f"},"Literature, Media and Communication":{"count":1,"color":"#d81a92"},"AE":{"count":1,"color":"#79e554"},"Music Technology":{"count":1,"color":"#e069d2"},"Biomedical engineering":{"count":1,"color":"#f43a3d"},"Chemical and Biomolecular Engineering":{"count":1,"color":"#114dff"},"Literature, Media, and Communication":{"count":1,"color":"#efa7ab"},"Psychology":{"count":1,"color":"#5b15db"}};
 
+        let figures = document.querySelectorAll('.art-crawl-item');
+        figures.forEach(figure => {
+            let major = figure.attributes.getNamedItem('data-major').value;
+            figure.style.backgroundColor = colors[major].color;
+        });
+
+        let params = getHashParams(window.location.hash.substr(1));
+        id = params.id;
+        let colorOnLoad = $('#' + id).css('background-color');
+        console.log(colorOnLoad);
+        $('#legend').css('background-color', colorOnLoad);
+
         $('#color-btn').click(function() {
             
-            let hlImg = document.querySelector('.highlight').firstElementChild;
-            let item  = document.querySelector('.art-crawl-item');
 
             $('.art-crawl-item > img').toggleClass('show-color');
-            $('#legend').toggleClass('hide');
-
-            let figures = document.querySelectorAll('.art-crawl-item');
-            figures.forEach(figure => {
-                let major = figure.attributes.getNamedItem('data-major').value;
-                figure.style.backgroundColor = colors[major].color;
-            });
+            //$('#legend').toggleClass('hide');
             
             let activeColor = $('.art-crawl-item.highlight').css('background-color');
             $('#legend').css('background-color', activeColor);
