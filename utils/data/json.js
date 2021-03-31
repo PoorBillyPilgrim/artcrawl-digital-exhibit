944d29668cf9f927267301efa1ae1d08e0280836
Tyler Jones <tjjones93@gmail.com>
2021-03-31 17:37:53 -0400

    handle combining 2021.json and 2020.json

--- utils/data/json.js
+++ utils/data/json.js
@@ -8,7 +8,7 @@ function write2021JSON() {
                 ResponseId: submissions[i].ResponseId,
                 "First Name": submissions[i]["First Name"],
                 "Last Name": submissions[i]["Last Name"],
-                "Username": submissions[i]["Username"],
+                "Username": submissions[i]["Username"].toLowerCase(),
                 "Major": submissions[i]["Major"],
                 "College": submissions[i]["College"],
                 "Year": "2021",
@@ -30,5 +30,8 @@ fs.writeFile('../../data/2021.json', write2021JSON(), (err) => {
     console.log(err);
 });
 
+/* Save this until after processing 2021 JSON
+    also need to remove duplicate media
 // copies data.json as 2020.json
 fs.copyFile('../../data/data.json', '../../data/2020.json', (err) => console.log(err))
+*/
