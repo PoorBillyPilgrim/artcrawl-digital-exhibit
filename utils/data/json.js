79225b29d53d72f47f889d19ff06243002b7ecd0
Tyler Jones <tjjones93@gmail.com>
2021-03-31 11:57:55 -0400

    create two json files for processing: 2020.json and 2021.json

---
+++ utils/data/json.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const submissions = require('../../data/qualtrics.json');
+const data = [];
+function write2021JSON() {
+    for (let i = 0; i < submissions.length; i++) {
+        data.push(
+            {
+                ResponseId: submissions[i].ResponseId,
+                "First Name": submissions[i]["First Name"],
+                "Last Name": submissions[i]["Last Name"],
+                "Username": submissions[i]["Username"],
+                "Major": submissions[i]["Major"],
+                "College": submissions[i]["College"],
+                "Year": "2021",
+                "Title of Artwork": submissions[i]["Title of Artwork"],
+                "Artist Statement": submissions[i]["Artist Statement"],
+                "Media": submissions[i]["Media"],
+                "Media URL": submissions[i]["Media URL"],
+                "Video Platform": submissions[i]["Video Platform"],
+                "Audio File": submissions[i]["Audio File_Name"],
+                "Image File": submissions[i]["Image File_Name"]
+            }    
+        );
+    }
+    return JSON.stringify(data, null, 2);
+}
+
+// creates 2021.json
+fs.writeFile('../../data/2021.json', write2021JSON(), (err) => {
+    console.log(err);
+});
+
+// copies data.json as 2020.json
+fs.copyFile('../../data/data.json', '../../data/2020.json', (err) => console.log(err))
