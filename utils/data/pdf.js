70be7afe3703197e6c3ec850dc654b7aa733991d
Tyler Jones <tjjones93@gmail.com>
2021-04-01 10:24:00 -0400

    add delete pdf file after conversion to promise chain

--- utils/data/pdf.js
+++ utils/data/pdf.js
@@ -6,14 +6,18 @@ const PDFImage = require("pdf-image").PDFImage; // https://www.npmjs.com/package
 data.forEach(sub => {
     let i = sub["Image File"].search(/([^.]*$)/),
     ext = sub["Image File"].slice(i).toLowerCase();
+    
     if (ext === "pdf") {
-        let pdfImage = new PDFImage(`../media/artcrawl/images/${sub["Image File"]}`);
+        let pdf = sub["ResponseId"] + '_' + sub["Image File"];
+        let pdfImage = new PDFImage(`../media/qualtrics/images/${pdf}`);
 
         pdfImage.convertPage(0).then(function (imagePath) {
-        // 0-th page (first page) of the slide.pdf is available as slide-0.png
-        fs.rename(imagePath, imagePath.replace(/-0/, ''), (err) => console.log(err)) 
-        return sub["Image File"];
-    }).then(pdf => console.log(pdf));
+            // 0-th page (first page) of the slide.pdf is available as slide-0.png
+            fs.rename(imagePath, imagePath.replace(/-0/, ''), (err) => console.log(err)); // removes '-0' that PDFImage adds
+            return pdf;
+        }).then(pdf => {
+            fs.unlink(`../media/qualtrics/images/${pdf}`, (err) => console.log(err)); // deletes .pdf file
+        })
     }
 });
 
