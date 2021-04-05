e342b82f3faf32f773a2dd4c9b69094436fdacd4
Tyler Jones <tjjones93@gmail.com>
2021-04-05 13:33:37 -0400

    clean up comments

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const express = require('express');
 const data = require('./data/data.json');
 const majors = require('./data/majors.json')
 const path = require('path');
-// const regex = /.(jpeg|jpg|png)/; this was needed for the actual art crawl submissions
 
 const port = process.env.PORT || 3000;
 
 const app = express();
-// Must match JSON keys exactly, capiatlize for single words and use bracket notation for keys > one word
 
 app.set('view engine', 'ejs');
 
@@ -21,11 +19,6 @@ app.get('/', (req, res) => {
     });
 });
 app.get('/audio/:id', (req, res) => {
-    /*data.forEach(x => {
-        if (x["Media URL"] === req.params.file) {
-            let file = file;
-        }
-    });*/
     res.render('audio', {
         data: data,
         id: req.params.id
